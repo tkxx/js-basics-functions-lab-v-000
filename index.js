a452dd1113aaa7fa9d42cfb3997283cc902ea39f
Tabitha Sin <tabitha.sin@gmail.com>
2018-02-08 20:35:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function calculatesFarePrice(start, destination) {
   else if (feet > 400 && feet < 2000) {
     return feet * 0.02
   }
-  else if (feet => 2000 && < 2500) {
+  else if (feet >= 2000 && < 2500) {
     return 25
   }
   else {
