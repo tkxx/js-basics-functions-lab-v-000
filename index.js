276b5f6208ef3e0488af6c3bd946da41d04d5e87
Tabitha Sin <tabitha.sin@gmail.com>
2018-02-08 20:25:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,19 +13,3 @@ function distanceTravelledInFeet (start, destination) {
   const feet = Math.abs(destination - start)*264
   return feet
 }
-
-function calculatesFarePrice(start, destination) {
-  const feet = distanceTravelledInFeet(start, destination)
-    if (feet <= 400) {
-      return 0
-    }
-    else if (feet > 400 && feet < 2000) {
-      return feet*0.02
-    }
-    else if (feet >= 2000 && feet <2500) {
-      return 25
-    }
-    else (feet > 2500) {
-      return 'cannot travel that far'
-    }
-}
